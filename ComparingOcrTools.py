31e05b00690035293ae205d88159325da4e936d1
Анна Сеславинская <anna-seslavinskaya@mail.ru>
2024-06-17 18:24:07 +0300

    actual

--- ComparingOcrTools.py
+++ ComparingOcrTools.py
@@ -1,3 +1,4 @@
+import configparser
 from datetime import datetime
 from glob import glob
 import Levenshtein
@@ -9,7 +10,10 @@ from IPython.display import display
 
 from OcrTools import get_text_tesseract, get_text_easyocr,get_text_keras
 
-
+config = configparser.ConfigParser()
+config.read("settings.ini")
+root = config.get("Paths", "work_dir_path")
+save_results_path = config.get("Paths", "save_results_path")
 def save_result_plot(image, data_tesseract, data_easyocr, data_kerasocr):
     figsize = [30, 20]
     fig, axs = plt.subplots(2, 2, figsize=(figsize))
@@ -29,7 +33,7 @@ def save_result_plot(image, data_tesseract, data_easyocr, data_kerasocr):
     time = datetime.now().strftime("%m_%d__%H_%M_%S")
     id = image.split("\\")[1].split(".")[0]
     plt.tight_layout()
-    plt.savefig(f'output/result_{id}_{time}.png')
+    plt.savefig(f'{save_results_path}/output/result_{id}_{time}.png')
 
 def result_string(records):
     words=[]
@@ -61,24 +65,27 @@ def process_image(image,annot=[]):
 
     #добавление времени распознавания в статистику
     image_statistic.extend([time1, time2, time3])
+    if annot!=[]:
+        #получение и добавление в статистику эталонного текста из датасета
+        records = annot[annot.image_id == id][['utf8_string']]
+        text_expected = result_string(records)
+        #image_statistic.append(text_expected)
 
-    #получение и добавление в статистику эталонного текста из датасета
-    records = annot[annot.image_id == id][['utf8_string']]
-    text_expected = result_string(records)
-    #image_statistic.append(text_expected)
-
-    #вычисление и добавление в статистику точности распознавания
-    for data_frame in [data_frame_1,data_frame_2,data_frame_3]:
-        accuracy = get_accuracy(data_frame,text_expected)
-        image_statistic.append(accuracy)
+        #вычисление и добавление в статистику точности распознавания
+        for data_frame in [data_frame_1,data_frame_2,data_frame_3]:
+            accuracy = get_accuracy(data_frame,text_expected)
+            image_statistic.append(accuracy)
+    else:
+        for data_frame in [data_frame_1,data_frame_2,data_frame_3]:
+            image_statistic.append("-")
     return [image_statistic,[data_frame_1,data_frame_2,data_frame_3]]
 
 def process_images_from_dataset():
-    annot = pd.read_parquet('input/annot.parquet')
-    imgs = pd.read_parquet('input/img.parquet')
-    img_fns = glob('input/random_images/*')
+    annot = pd.read_parquet(f'{root}/input/annot.parquet')
+    imgs = pd.read_parquet(f'{root}/input/img.parquet')
+    img_fns = glob(f'{root}/input/random_images/*')
     table = []
-    for i in range(1, 2):
+    for i in range(1, 3):
         image = img_fns[i]
         statistic,data_frames=process_image(image,annot)
         table.append(statistic)
@@ -86,6 +93,16 @@ def process_images_from_dataset():
     data_frame_results = pd.DataFrame(table, columns=['id','tesseract_time', 'easyocr_time', 'keras_ocr_time','tesseract_accuracy', 'easyocr_accuracy', 'keras_ocr_accuracy'])
     display(data_frame_results.to_string())
 
+def process_images_from_folder(folder):
+    img_fns = glob(f'{folder}/*')
+    table = []
+    for i in range(0, len(img_fns)):
+        image = img_fns[i]
+        statistic,data_frames=process_image(image)
+        table.append(statistic)
+        save_result_plot(image,*data_frames)
+    data_frame_results = pd.DataFrame(table, columns=['id','tesseract_time', 'easyocr_time', 'keras_ocr_time','tesseract_accuracy', 'easyocr_accuracy', 'keras_ocr_accuracy'])
+    display(data_frame_results.to_string())
 if __name__ == '__main__':
-    process_images_from_dataset()
+    process_images_from_folder(f'{root}/input/interface')
     
